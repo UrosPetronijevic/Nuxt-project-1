e260c5ee055f76796da1ebe41725e47652e57b4d
UrosPetronijevic <ukipetlja@gmail.com>
2026-03-31 06:36:06 -0400

    added pages and slug page

---
+++ app/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Hello world</h1>
+  <NuxtLink to="/about">About</NuxtLink>
+</template>
+
+<script lang="ts" setup>
+useHead({
+  title: "My nuxt app",
+});
+</script>
---
+++ app/pages/profiles/[profileId].vue
@@ -0,0 +1,5 @@
+<template>{{ profileId }}</template>
+
+<script lang="ts" setup>
+const profileId = useRoute().params.profileId;
+</script>
